746cd3a4ac4c6a27a6f019fcbc8335f76d415787
shathakh <shathaqudaih17@gmail.com>
2022-09-27 13:16:08 +0300

    create footer component relates #53

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import './Footer.css';
+import Link from 'react-router-dom';
+
+const Footer = () => {
+  <footer className="footer">
+    <div className="content">
+      <div className="menu">
+        <h4>MENU</h4>
+        <Link to="/">Home</Link>
+        <Link to="/">Shop</Link>
+        <Link to="/">About</Link>
+        <Link to="/">Contact</Link>
+      </div>
+      <div className="shop">
+        <h4>SHOP</h4>
+        <Link to="/">Category 1</Link>
+        <Link to="/">Category 2</Link>
+        <Link to="/">Category 3</Link>
+        <Link to="/">Category 4</Link>
+      </div>
+      <div className="help">
+        <h4>HELP</h4>
+        <Link to="/">Privacy Policy</Link>
+        <Link to="/">Terms</Link>
+        <Link to="/">Shipping</Link>
+        <Link to="/">Secure</Link>
+      </div>
+      <div>
+        <span>(052) 611-5711</span>
+        <span>company@domain.com</span>
+      </div>
+      <div>
+        <h2>Company</h2>
+      </div>
+    </div>
+    <div className="icon">
+      <i className="fa-brands fa-facebook-f" />
+      <i className="fa-brands fa-linkedin-in" />
+      <i className="fa-brands fa-instagram" />
+    </div>
+  </footer>;
+};
+
+export default Footer;
