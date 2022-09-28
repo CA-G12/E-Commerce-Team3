62fb6146db6938c6b49fcd51d660debfe3eab231
ysalem-dev-89 <yazeed.salem.it@gmail.com>
2022-09-28 09:32:31 +0300

    added fixed components for product details and car Relates #69

--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -5,6 +5,8 @@ import Root from '../root';
 // import PageTitle from './components/PageTitle/PageTitle';
 // import SecondHeader from './components/SecondHeader/SecondHeader';
 import ProductsContainer from '../components/Pages/ProductsContainer/ProductsContainer';
+import ProductDetails from '../components/Pages/ProductDetails/ProductDetails.jsx';
+import Cart from '../components/Pages/Cart/Cart.jsx';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,8 @@ const router = createBrowserRouter([
     children: [
       { index: '/', element: <ProductsContainer /> },
       { path: '/products', element: <ProductsContainer /> },
+      { path: '/products/:id', element: <ProductDetails /> },
+      { path: '/cart', element: <Cart /> },
     ],
   },
 ]);
